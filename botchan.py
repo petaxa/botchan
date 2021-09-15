5b253c0984f6d53d379e9fe9c2b9e39d5656b4e7
tempestif <50777725+tempestif@users.noreply.github.com>
2021-09-15 13:53:28 +0900

    commit

--- botchan.py
+++ botchan.py
@@ -56,8 +56,7 @@ async def on_voice_state_update(member, before, after):
                 await alert_channel.send(msg)
             elif after.channel is None:
                 msg = f'{now:%m/%d-%H:%M} に {member.name} が {before.channel.name} から退出しました。'
-                await alert_channel.send(msg)
-                
+                await alert_channel.send(msg)     
       elif member.guild.id == 884594072130560120 and (before.channel != after.channel):
         now = datetime.utcnow() + timedelta(hours=9)
         alert_channel_M = client.get_channel(887357758523985920)
